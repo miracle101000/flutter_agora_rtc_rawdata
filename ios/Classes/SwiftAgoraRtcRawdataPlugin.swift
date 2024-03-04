8b939238b451a525ae2d129b4962df3650f06bdc
miracle101000 <okolomiracle76@outlook.com>
2024-03-04 13:58:26 +0100

    face unity integration

--- ios/Classes/SwiftAgoraRtcRawdataPlugin.swift
+++ ios/Classes/SwiftAgoraRtcRawdataPlugin.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import FURenderKit
 
 public class SwiftAgoraRtcRawdataPlugin: NSObject, FlutterPlugin, AgoraAudioFrameDelegate, AgoraVideoFrameDelegate {
     public static func register(with registrar: FlutterPluginRegistrar) {
@@ -67,14 +68,83 @@ public class SwiftAgoraRtcRawdataPlugin: NSObject, FlutterPlugin, AgoraAudioFram
     }
 
     public func onCaptureVideoFrame(_ sourceType: Int32, frame videoFrame: AgoraVideoFrame) -> Bool {
-        memset(videoFrame.uBuffer, 0, Int(videoFrame.uStride * videoFrame.height) / 2)
-        memset(videoFrame.vBuffer, 0, Int(videoFrame.vStride * videoFrame.height) / 2)
+        // memset(videoFrame.uBuffer, 0, Int(videoFrame.uStride * videoFrame.height) / 2)
+        // memset(videoFrame.vBuffer, 0, Int(videoFrame.vStride * videoFrame.height) / 2)
+         if(fuIsLibraryInit() > 0){
+            let input = FURenderInput.init();
+            if (videoFrame.type == .YUV420) {
+                //默认图片内部的人脸始终是朝上，旋转屏幕也无需修改该属性。
+                var inputOriginTation: FUImageOrientation = FUImageOrientationUP;
+                
+                switch videoFrame.rotation {
+                case 0:
+                    inputOriginTation = FUImageOrientationUP
+                case 90:
+                    inputOriginTation = FUImageOrientationLeft
+                case 180:
+                    inputOriginTation = FUImageOrientationDown
+                case 270:
+                    inputOriginTation = FUImageOrientationRight
+                default:
+                    inputOriginTation = FUImageOrientationLeft
+                }
+                input.renderConfig.imageOrientation = inputOriginTation;
+                //开启重力感应，内部会自动计算正确方向，设置fuSetDefaultRotationMode，无须外面设置
+                input.renderConfig.gravityEnable = true
+                //如果来源相机捕获的图片一定要设置，否则将会导致内部检测异常
+                input.renderConfig.isFromFrontCamera = true
+                //该属性是指系统相机是否做了镜像: 一般情况前置摄像头出来的帧都是设置过镜像，所以默认需要设置下。如果相机属性未设置镜像，改属性不用设置。
+                //实际测试发现图片帧没有镜像所以此属性设置false
+                input.renderConfig.isFromMirroredCamera = false
+                //贴纸强制左右镜像
+                input.renderConfig.stickerFlipH = true;
+                
+                let pointY: UnsafeMutablePointer<UInt8> = transform(videoFrame.yBuffer)
+                let pointU: UnsafeMutablePointer<UInt8> = transform(videoFrame.uBuffer)
+                let pointV: UnsafeMutablePointer<UInt8> = transform(videoFrame.vBuffer)
+                let imagebuffer = FUImageBufferMakeI420(pointY , pointU, pointV, Int(videoFrame.width),Int(videoFrame.height), Int(videoFrame.yStride), Int(videoFrame.uStride), Int(videoFrame.vStride))
+                input.imageBuffer = imagebuffer;
+                
+                if (FURenderKit.share().beauty != nil && FURenderKit.share().beauty?.enable == true) {
+                    if (Int(FURenderKit.devicePerformanceLevel().rawValue) >= Int(FUDevicePerformanceLevel.high.rawValue)) {
+                        let score = FUAIKit.fuFaceProcessorGetConfidenceScore(0)
+                        if (score > 0.95) {
+                            FURenderKit.share().beauty?.blurType = 3;
+                            FURenderKit.share().beauty?.blurUseMask = true;
+                        } else {
+                            FURenderKit.share().beauty?.blurType = 2;
+                            FURenderKit.share().beauty?.blurUseMask = false;
+                        }
+                    } else {
+                        FURenderKit.share().beauty?.blurType = 2;
+                        FURenderKit.share().beauty?.blurUseMask = false;
+                    }
+
+                }
+                FURenderKit.share().render(with: input)
+            }
+              }
         return true
     }
 
     public func onRenderVideoFrame(_ videoFrame: AgoraVideoFrame, uid _: UInt) -> Bool {
-        memset(videoFrame.uBuffer, 255, Int(videoFrame.uStride * videoFrame.height) / 2)
-        memset(videoFrame.vBuffer, 255, Int(videoFrame.vStride * videoFrame.height) / 2)
+        // memset(videoFrame.uBuffer, 255, Int(videoFrame.uStride * videoFrame.height) / 2)
+        // memset(videoFrame.vBuffer, 255, Int(videoFrame.vStride * videoFrame.height) / 2)
         return true
+    }
+
+     private func transform(_ buffer: UnsafeMutableRawPointer) -> UnsafeMutablePointer<UInt8> {
+        let point: UnsafeMutableRawPointer = buffer
+        let opaquePtr = OpaquePointer(point)
+        let baseAddress = UnsafeMutablePointer<UInt8>(opaquePtr)
+        return baseAddress
+    }
+    
+    private func untransform(_ byte:UnsafeMutablePointer<UInt8>) -> UnsafeMutableRawPointer {
+        let point: UnsafeMutablePointer<UInt8> = byte;
+        let opaquePtr = OpaquePointer(point);
+        let voidPoint = UnsafeMutableRawPointer(opaquePtr);
+        
+        return voidPoint
     }
 }
